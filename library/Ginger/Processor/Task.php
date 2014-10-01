b6f741c2c72ec121fca0a91a1348f5d6e8c26d46
Codeliner <kontakt@codeliner.ws>
2014-10-01 21:10:48 +0200

    Add concept for Tasklist and Task

---
+++ library/Ginger/Processor/Task.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the Ginger Workflow Framework.
+ * (c) Alexander Miertsch <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 30.09.14 - 22:32
+ */
+
+namespace Ginger\Processor;
+
+/**
+ * Class Task
+ *
+ * @package Ginger\Processor
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class Task 
+{
+    //a task can be: send command on commandBus xyz
+    //or publish event on eventBus xyz
+}
+ 
---
+++ library/Ginger/Processor/TaskList.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the Ginger Workflow Framework.
+ * (c) Alexander Miertsch <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 30.09.14 - 22:23
+ */
+
+namespace Ginger\Processor;
+
+class TaskList extends \SplQueue
+{
+
+}
+ 
