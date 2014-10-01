e42490eab6259026187480bfb56d5117775bdea1
Codeliner <kontakt@codeliner.ws>
2014-10-01 21:10:21 +0200

    Add WorkflowEngine skeleton

---
+++ library/Ginger/Processor/WorkflowEngine.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the Ginger Workflow Framework.
+ * (c) Alexander Miertsch <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 30.09.14 - 21:23
+ */
+
+namespace Ginger\Processor;
+
+/**
+ * Class WorkflowEngine
+ *
+ * Provides necessary infrastructure for a Process to run accordingly.
+ *
+ * @package Ginger\Processor
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class WorkflowEngine 
+{
+
+}
+ 
